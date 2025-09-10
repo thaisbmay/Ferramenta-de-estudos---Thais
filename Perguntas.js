3024dfb50d76c37ea22ab21320c5bd8339b69577
thaisbmay <may.thais@escola.pr.gov.br>
2025-09-10 12:30:16 +0000

    Fizemos 9 cartoes no js

---
+++ Perguntas.js
@@ -0,0 +1,53 @@
+criaCartao(
+    'Programação',
+    'O que é HTML?',
+    resposta
+)
+
+criaCartao(
+    'Programação',
+    'O que é CSS?',
+    resposta
+)
+
+criaCartao(
+    'Programação',
+    'O que é JavaScript?',
+    resposta
+)
+
+criaCartao(
+    'Programação',
+    'Para que serve a tag article?',
+    resposta
+)
+
+criaCartao(
+    'Programação',
+    'O que é a tag footer?',
+    resposta
+)
+
+criaCartao(
+    'Programação',
+    'Qual a diferença entre a tag tittle que usamos na tag head e a tag h1 que usamos na tag body?',
+    resposta
+)
+
+criaCartao(
+    'Programação',
+    'Qual a diferença entre as tags head e header?',
+    resposta
+)
+
+criaCartao(
+    'Programação',
+    'Para que serve o :root utilizado no CSS',
+    resposta
+)
+
+criaCartao(
+    'Programação',
+    'Qual a diferença entre margin e padding?',
+    resposta
+)
