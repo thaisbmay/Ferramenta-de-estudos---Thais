622a090be2a1dd9b28540de00b2fe3b92a8c2928
thaisbmay <may.thais@escola.pr.gov.br>
2025-09-10 13:35:32 +0000

    Criamos os js 7

--- Perguntas.js
+++ Perguntas.js
@@ -1,53 +1,53 @@
 criaCartao(
     'Programação',
     'O que é HTML?',
-    resposta
+    'É uma linguagem de programação que nos permite criar a estrutura do nosso site'
 )
 
 criaCartao(
     'Programação',
     'O que é CSS?',
-    resposta
+    'É uma linguagem de programação utilizada para dar estilo á página'
 )
 
 criaCartao(
     'Programação',
     'O que é JavaScript?',
-    resposta
+    'O javascript (JS) é uma linguagem de programação que nos permite interagir com a pagina'
 )
 
 criaCartao(
     'Programação',
     'Para que serve a tag article?',
-    resposta
+    'Para criar um conteúdo ou elemento independente. No nosso caso estamos usando para criar os cartões de estudo'
 )
 
 criaCartao(
     'Programação',
     'O que é a tag footer?',
-    resposta
+    'É  a tag utilizada para criar o rodapé'
 )
 
 criaCartao(
     'Programação',
     'Qual a diferença entre a tag tittle que usamos na tag head e a tag h1 que usamos na tag body?',
-    resposta
+    'A tag tittle aparecerá na aba da página que será aberta e a tag de titulo h1 é usada no corpo do site como titulo principal da página'
 )
 
 criaCartao(
     'Programação',
     'Qual a diferença entre as tags head e header?',
-    resposta
+    'head é tag de configurações, tudo que fizermos dentro dela, não aparecerá na página do nosso site. Header é tag de cabeçalho da página'
 )
 
 criaCartao(
     'Programação',
     'Para que serve o :root utilizado no CSS',
-    resposta
+    'Serve para salvar variáveis para serem utilizadas durante o desenvolvimento da página, como por exemplo, cores e fontes'
 )
 
 criaCartao(
     'Programação',
     'Qual a diferença entre margin e padding?',
-    resposta
+    'Margin é espaçamento externo enqaunto o padding é espaçamento interno'
 )
